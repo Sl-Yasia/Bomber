adaa9fa80e01f3cfd00cc72942210586416264fb
Yasiya <85439062+Sl-Yasia@users.noreply.github.com>
2022-07-28 11:25:23 +0530

    1.0v⏳

---
+++ update.sh
@@ -0,0 +1,12 @@
+echo ""
+echo -e "\e[1;37m[*] \e[1;33mUpdating... ⏳"
+cd
+rm -rf Bomber
+echo ""
+git clone https://github.com/Sl-Yasia/Bomber.git
+echo ""
+echo -e "\e[1;37m[*] \e[1;33mUpdate Successful... ✅"
+echo ""
+echo -e "\e[1;37m[*] \e[1;32;40mRun Bomber Again...🔄"
+echo ""
+cd
